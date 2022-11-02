05e034250fbc53111e105a2c20a408d579e0527d
Samanvya Tripathi <samanu09@gmail.com>
2022-11-02 19:46:03 -0400

    Change javadocs to zip

--- stocks-code/res/javadocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"ClientRunner"},{"p":"controller","l":"Handler"},{"p":"controller","l":"InteractionHandler"},{"p":"model","l":"ModelOrchestrator"},{"p":"model","l":"Orchestrator"},{"p":"view","l":"UserInteraction"},{"p":"view","l":"ViewHandler"}]
