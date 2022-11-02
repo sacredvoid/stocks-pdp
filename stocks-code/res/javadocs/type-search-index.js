f15f1f09b3911fdf32e1ec7980333af7dea5ed31
Aakash Mahesha <aakash280500@gmail.com>
2022-11-02 19:14:26 -0400

    Add javadocs, class diag

---
+++ stocks-code/res/javadocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"ClientRunner"},{"p":"controller","l":"Handler"},{"p":"controller","l":"InteractionHandler"},{"p":"model","l":"ModelOrchestrator"},{"p":"model","l":"Orchestrator"},{"p":"view","l":"UserInteraction"},{"p":"view","l":"ViewHandler"}]
