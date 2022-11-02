13e6a9721bb35ca9f8f2c63ed3999bba43a76cd3
Samanvya Tripathi <samanu09@gmail.com>
2022-11-01 20:46:59 -0400

    resolve merge conf

--- stocks-code/test/controller/InteractionHandlerTest.java
+++ stocks-code/test/controller/InteractionHandlerTest.java
@@ -1,4 +1,57 @@
 package controller;
 
+import model.ModelOrchestrator;
+import org.junit.Before;
+import org.junit.Test;
+import view.UserInteraction;
+
+import java.io.*;
+import java.util.NoSuchElementException;
+
+import static org.junit.Assert.assertEquals;
+
 public class InteractionHandlerTest {
+
+    ModelOrchestrator morch;
+    UserInteraction ui;
+    InteractionHandler ih;
+    @Before
+    public void setup() {
+        morch = new ModelOrchestrator();
+        ui = new UserInteraction();
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void testHomePage() {
+        Reader in = new StringReader(" ");
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        PrintStream out = new PrintStream(bytes);
+        ih = new InteractionHandler(
+                morch,
+                ui,
+                in,
+                out
+        );
+        ih.run();
+
+        String expected = "";
+        assertEquals(expected, bytes.toString());
+    }
+
+    @Test
+    public void testLoadPortfolio() {
+        Reader in = new StringReader("1");
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        PrintStream out = new PrintStream(bytes);
+        ih = new InteractionHandler(
+                morch,
+                ui,
+                in,
+                out
+        );
+        ih.run();
+
+        String expected = "";
+        assertEquals(expected, bytes.toString());
+    }
 }
