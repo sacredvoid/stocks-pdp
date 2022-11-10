a0249571632fc9bfdce51b1c7e68a61d2a9bdc1a
Samanvya Tripathi <samanu09@gmail.com>
2022-11-10 15:31:33 -0500

    Fix pretty print

--- stocks-code/src/model/fileops/JSONFileOps.java
+++ stocks-code/src/model/fileops/JSONFileOps.java
@@ -2,7 +2,9 @@ package model.fileops;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -40,7 +42,8 @@ public class JSONFileOps extends AFileOps {
   public void writeToFile(String filename, String dir, String data) throws IOException {
     String path = pathResolver(filename,dir);
     try (Writer writer = Files.newBufferedWriter(Path.of(path), StandardCharsets.UTF_8)) {
-      customJSONReader.toJson(data,writer);
+      JsonElement json = JsonParser.parseString(data);
+      customJSONReader.toJson(json,writer);
     }
     catch (Exception e) {
       throw new IOException(e.getMessage());
@@ -50,13 +53,8 @@ public class JSONFileOps extends AFileOps {
   public static void main(String[] args) throws IOException {
     JSONFileOps j = new JSONFileOps();
     String out = j.readFile("1234.json","PortfolioData");
-    // Custom type Hashmap<String,PortfolioData>
-//    Type
     Type typetoken = new TypeToken<HashMap<String,PortfolioData>>() {}.getType();
     Map<String,PortfolioData> pf = new Gson().fromJson(out,typetoken);
-//    for(Entry<String, PortfolioData> entry:pf.entrySet()){
-//      entry.getValue().ge;
-//    }
     System.out.printf("****\n%s****\n",pf.get("2020-10-11").getStockList().get(0).getStockName());
     System.out.println(out);
     j.writeToFile("2345.json","PortfolioData",out);
