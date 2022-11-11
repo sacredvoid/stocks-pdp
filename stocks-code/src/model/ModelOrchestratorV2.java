703ddf01473da2f1a062d93e76f41df9450b916a
Samanvya Tripathi <samanu09@gmail.com>
2022-11-11 03:54:26 -0500

    Get portfolio by date and ID implemented, value plug-in WIP

--- stocks-code/src/model/ModelOrchestratorV2.java
+++ stocks-code/src/model/ModelOrchestratorV2.java
@@ -4,21 +4,16 @@ import com.google.gson.Gson;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.stream.Collectors;
 import model.fileops.FileOps;
 import model.fileops.JSONFileOps;
 import model.portfolio.CSVToPortfolioAdapter;
 import model.portfolio.PortfolioData;
 import model.portfolio.PortfolioDataAdapter;
 import model.portfolio.PortfolioToCSVAdapter;
+import model.portfolio.StockData;
 import model.portfolio.Utility;
-import model.portfolio.filters.FilterPortfolio;
 
 
 public class ModelOrchestratorV2 extends AOrchestrator {
@@ -58,9 +53,23 @@ public class ModelOrchestratorV2 extends AOrchestrator {
   public String getPortfolioValueByID(String date, String pfID) throws FileNotFoundException {
     String pfData = jsonParser.readFile(pfID + ".json", PORTFOLIO_DATA_PATH);
     Map<String, PortfolioData> parsedPFData = PortfolioDataAdapter.getObject(pfData);
-    Map<String, PortfolioData> filteredData = FilterPortfolio.getPortfolioAfterDate(parsedPFData,date);
-//    System.out.println();
-    return filteredData.toString();
+
+//    // Example for filtering data
+//    Map<String, PortfolioData> filteredData = FilterPortfolio.getPortfolioAfterDate(parsedPFData,date);
+    // Send this stockDataCSV to Aakash
+    List<StockData> stockDataForDate;
+    try {
+      stockDataForDate = parsedPFData.getOrDefault(date,null).getStockList();
+    }
+    catch (NullPointerException n) {
+      return "Sorry, no data for given date.";
+    }
+    if(stockDataForDate != null) {
+      String stockDataCSV = PortfolioToCSVAdapter.buildStockQuantityList(stockDataForDate);
+      return stockDataCSV;
+    }
+    else return "Sorry, no stocks for given date.";
+    //    System.out.println();
   }
 
   @Override
